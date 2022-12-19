2e391a74bd47999d854c57b1af90bc1ef058e8c5
nicolassallerin <nicolas.sallerin@oclock.io>
2022-12-19 10:01:15 +0100

    Deuxième interaction : fermer la modale

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,21 +10,39 @@ const app = {
   },
   
   addListenerToActions() {
-    // On récupère le bouton
-    const addListBtnElmt = document.getElementById("addListButton");
+    // On récupère le bouton d'ouverture de la modale
+    const addListBtnElmt = document.getElementById('addListButton');
     
-    // On ajoute l'écouteur sur le cl
+    // On ajoute l'écouteur sur le clique
     addListBtnElmt.addEventListener('click', app.showAddListModal);
+
+    // On ajoute également les écouteurs pour les boutons qui ferment la modale
+    const modalCloseButtonElmtList = document.querySelectorAll('.close');
+    // On oublie pas que querySelectroAll renvoie un tableau d'éléments, et qu'il faut donc boucler dessus
+    for(const closeButtonElmt of modalCloseButtonElmtList) {
+      closeButtonElmt.addEventListener('click', app.hideModals);
+    }
+
+    // On pourrait aussi faire la meme chose avec un foreach directement
+    // modalCloseButtonElmtList.forEach((closeButtonElmt) => closeButtonElmt.addEventListener('click', app.hideModals));
   },
 
   showAddListModal() {
     // On récupère la div de la modale
-    const modaleDivElmt = document.getElementById("addListModal");
+    const modaleDivElmt = document.getElementById('addListModal');
 
     // On ajoute la classe is-active
     modaleDivElmt.classList.add('is-active');
+  },
+
+  hideModals() {
+    // Pour faire disparaitre une modale, il suffit de supprimer sa classe "is-active"
+    const allModalsElmtList = document.querySelectorAll('.modal');
+
+    for(const modalElmt of allModalsElmtList) {
+      modalElmt.classList.remove('is-active');
+    }
   }
-  
 };
 
 
